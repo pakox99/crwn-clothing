0a97960dcaa12354970537e4a573afd60561956a
aymen <36508782+pakox99@users.noreply.github.com>
2021-11-06 19:31:48 +0100

    finished styling menu item comp

---
+++ src/pages/homepage/hompage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../components/directoty/directory.component";
+import MenuItem from "../../components/menuitem/menu-item.component";
+import './homepage.styles.scss';
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory/>
+
+
+    </div>
+);
+export default HomePage;
